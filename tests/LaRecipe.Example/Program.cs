306e7abcc92e632bbd50ae62e034dce6ed9850d9
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-14 00:02:09 +0400

    wip

--- tests/LaRecipe.Example/Program.cs
+++ tests/LaRecipe.Example/Program.cs
@@ -1,6 +1,19 @@
+using LaRecipe;
+using LaRecipe.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddLaRecipe()  // <- 
+    .AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection()
+    .UseLaRecipe()  // <- 
+    .UseAuthorization();
+
+app.MapControllers();
+app.MapGet("/", () => "Visit /docs 🎉");
 
 app.Run();
