a5e432ef4b5363f969e6946983e6a2425328069c
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-13 20:48:08 +0400

    add example

--- src/LaRecipe/DocumentationResolver.cs
+++ src/LaRecipe/DocumentationResolver.cs
@@ -7,7 +7,7 @@ public class DocumentationResolver: IDocumentationResolver
 {
     public string Resolve()
     {
-        using var reader = new StreamReader(@"../../docs/overview.md");
+        using var reader = new StreamReader(@"Documentation/overview.md");
         var stringBuilder = new StringBuilder(reader.ReadToEnd());
 
         return Markdown.ToHtml(stringBuilder.ToString());
