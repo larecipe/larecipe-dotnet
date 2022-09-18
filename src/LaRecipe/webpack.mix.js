a004cf852076a4905feee6ac972c13b54bf18f15
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-18 09:20:49 +0400

    update assets

--- src/LaRecipe/webpack.mix.js
+++ src/LaRecipe/webpack.mix.js
@@ -1,4 +1,6 @@
 let mix = require('laravel-mix');
 
 mix.copy('node_modules/@larecipe/larecipe-ui/dist/app.js', 'Web')
-    .copy('node_modules/@larecipe/larecipe-ui/dist/app.css', 'Web');
+    .copy('node_modules/@larecipe/larecipe-ui/dist/app.css', 'Web')
+    .setPublicPath("Web")
+    .version();
