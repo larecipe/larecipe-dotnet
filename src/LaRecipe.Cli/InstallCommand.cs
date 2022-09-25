b2a1be29e98c622e335d0557bde4955345c66291
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-25 16:34:00 +0400

    create CLI tool

---
+++ src/LaRecipe.Cli/InstallCommand.cs
@@ -0,0 +1,23 @@
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+
+namespace LaRecipe.Cli;
+
+public static class InstallCommand
+{
+    public static Command Create()
+    {
+        var command = new Command("install", "Create initial documentation files.");
+        
+        command.Handler = CommandHandler.Create<InitCommandArguments>(args =>
+        {
+            Console.WriteLine("Created Documentation Files.");
+        });
+
+        return command;
+    }
+    
+    private class InitCommandArguments
+    {
+    }
+}
---
+++ src/LaRecipe.Cli/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using LaRecipe.Cli;
+
+var command = new RootCommand
+{
+    Description = "LaRecipe Commands",
+};
+
+command.AddCommand(InstallCommand.Create());
+
+var builder = new CommandLineBuilder(command);
+builder.UseHelp();
+builder.UseParseErrorReporting();
+
+builder.CancelOnProcessTermination();
+
+var parser = builder.Build();
+parser.InvokeAsync(args);
+
