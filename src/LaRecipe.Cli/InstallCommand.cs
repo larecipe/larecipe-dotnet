dd615bea14050f8e9ef3dd3ec28602b0985749d8
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-25 17:55:19 +0400

    add stubs

--- src/LaRecipe.Cli/InstallCommand.cs
+++ src/LaRecipe.Cli/InstallCommand.cs
@@ -1,15 +1,27 @@
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
+using System.Reflection;
+using Microsoft.Extensions.FileProviders;
 
 namespace LaRecipe.Cli;
 
 public static class InstallCommand
 {
+    private const string EmbeddedFileNamespace = "LaRecipe.Cli";
+    
     public static Command Create() =>
         new ("install", "Create initial documentation files.")
         {
             Handler = CommandHandler.Create<InitCommandArguments>(_ =>
             {
+                var fileProvider = new EmbeddedFileProvider(typeof(InstallCommand).GetTypeInfo().Assembly, "LaRecipe.Cli.Stubs");
+                var stubs = fileProvider.GetDirectoryContents(@"");
+                
+                foreach (var stub in stubs)
+                {
+                    File.Create($"Documentation/{stub.Name}");
+                }
+                
                 Console.WriteLine("Created documentation files.");
             })
         };
---
+++ src/LaRecipe.Cli/Stubs/overview.md
@@ -0,0 +1,37 @@
+# Overview
+
+Write here a nice summary of the current page..
+
+---
+
+[TOC]
+
+## Overview
+
+
+This is a nice empty meaning sentence.
+
+
+```php
+echo 'LaRecipe is beautiful';
+```
+
+## Features
+
+Inline code is `cool`
+
+
+```js
+echo 'LaRecipe is beautiful';
+```
+
+
+FinalMessageResolver | <larecipe-card><larecipe-badge type="success" circle class="mr-2" icon="fa fa-book">🚀</larecipe-badge> Yay! you saved AED 10 on delivery!<larecipe-progress type="success" :value="100"></larecipe-progress></larecipe-card>
+:- | :-
+0  | 1
+
+
+<larecipe-card>
+    <larecipe-badge type="success" circle class="mr-2" icon="fa fa-book"></larecipe-badge> Markdown Support
+    <larecipe-progress type="success" :value="100"></larecipe-progress>
+</larecipe-card>
